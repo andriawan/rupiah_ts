a106f445b42f4ba6a1319fa558b5758e5e2002e4
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-05-24 20:20:49 +0700

    add simple test

---
+++ mod_test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import Rupiah from "./rupiah.ts";
+
+Deno.test("test correct format function", () => {
+    let rupiah = new Rupiah(2000);
+  assertEquals(rupiah.format, 'Rp. 2.000,00');
+});
