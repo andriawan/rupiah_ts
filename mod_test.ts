a18d1d72792558816eb39da3ae6ac773111bd226
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-05-25 19:19:51 +0700

    formatting code test

--- mod_test.ts
+++ mod_test.ts
@@ -2,11 +2,11 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import Rupiah from "./rupiah.ts";
 
 Deno.test("test correct format function", () => {
-    let rupiah = new Rupiah(2000);
-  assertEquals(rupiah.format, 'Rp. 2.000,00');
+  let rupiah = new Rupiah(2000);
+  assertEquals(rupiah.format, "Rp. 2.000,00");
 });
 
 Deno.test("test correct terbilang function", () => {
-    let rupiah = new Rupiah(2000);
-  assertEquals(rupiah.terbilang, 'Dua Ribu Rupiah');
+  let rupiah = new Rupiah(2000);
+  assertEquals(rupiah.terbilang, "Dua Ribu Rupiah");
 });
