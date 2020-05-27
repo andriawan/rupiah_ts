496769ac3d271927a7cccd6aa6d9a88ff4c71d89
Muh Isfhani Ghiath <misfahani3@gmail.com>
2020-05-27 15:14:00 +0700

    feat(rupiah:test): reformat Rp

--- mod_test.ts
+++ mod_test.ts
@@ -3,7 +3,7 @@ import Rupiah from "./rupiah.ts";
 
 Deno.test("test correct format function", () => {
   let rupiah = new Rupiah(2000);
-  assertEquals(rupiah.format, "Rp. 2.000,00");
+  assertEquals(rupiah.format, "Rp2.000,00");
 });
 
 Deno.test("test correct terbilang function", () => {
